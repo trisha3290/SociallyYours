3c0a9e537f5998d737d281b6ddff0ab11bfc140e
Sahil <g.sahil@iitg.ac.in>
2022-05-28 01:48:01 +0530

    font-awesome

--- public/chat.js
+++ public/chat.js
@@ -79,7 +79,7 @@ export default class Chat{
 
     injectHTML(){
         this.chatWrapper.innerHTML = `
-        <div class="chat-title-bar">Chat <span class="chat-title-bar-close"><i class="fas fa-times-circle"></i></span></div>
+        <div class="chat-title-bar">Chat <span class="chat-title-bar-close">&#10060;/span></div>
         <div id="chat" class="chat-log"></div>
         <form id="chatForm" class="chat-form border-top">
       <input type="text" class="chat-field" id="chatField" placeholder="Type a message…" autocomplete="off">
